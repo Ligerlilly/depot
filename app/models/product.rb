7c2bd7144df8b3d179dcb8563b7e95c01e295d1b
Jason <quizathaderat@yahoo.com>
2013-05-01 12:28:28 -0700

    Add cart button

--- app/models/product.rb
+++ app/models/product.rb
@@ -6,4 +6,19 @@ class Product < ActiveRecord::Base
   validates :image_url, allow_blank: true, format: { 
             with: %r{\.(gif|jpg|png)$}i,
             message: 'must be a URL for GIF, JPG, or PNG' }
+  has_many :line_items
+  before_destroy :ensure_not_referenced_by_any_line_item
+
+  private
+
+  #ensure that there are no line items referencing this product
+  def ensure_not_referenced_by_any_line_item
+    if line_items.empty?
+       return true
+    else
+       errors.add(:base, 'Line Items present')
+       return false
+    end
+  end
+  
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,21 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130418220256) do
+ActiveRecord::Schema.define(:version => 20130430195338) do
+
+  create_table "carts", :force => true do |t|
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
+
+  create_table "line_items", :force => true do |t|
+    t.integer  "product_id"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+    t.integer  "cart_id"
+  end
+
+  add_index "line_items", ["cart_id"], :name => "index_line_items_on_cart_id"
 
   create_table "products", :force => true do |t|
     t.string   "title"
